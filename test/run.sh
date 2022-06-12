b09496f334c26a7e5d1748c6e8b1589596214e18
aescobar <adan@codeits.cl>
2022-06-12 19:14:20 -0400

    freeze dependencies

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,6 @@
 
-export MONGODB_URI=$DB_TEST
+export MONGODB_URI=mongodb+srv://aescobar:Tks12345@cluster0.iz5q7.mongodb.net/test
+echo "MONGODB_URI=$MONGODB_URI"
 pytest -v --cov /api-run/test/
 
 echo "Testting Done"
