d84df50f1c7cc730a1038095b3d94a1ee188e3a1
aescobar <adan@codeits.cl>
2022-06-21 18:06:15 -0400

    relationship join

--- lib/db/sql/util.py
+++ lib/db/sql/util.py
@@ -1,14 +1,94 @@
 from operator import or_,and_
-from pydoc import resolve
 import time
-from sqlalchemy import func
+import  sqlalchemy
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+from sqlalchemy.inspection import inspect
+
+from lib.log.UtilLog import UtilLog
 from lib.db.sql.connection import SqlConnection
 
 db = SqlConnection.getSqlAlchemy()
 
-class UtilSQLAlchemy:
+
+#customer:{name:{like:ad}}
+class RelationshipInspector:
+	model=None
+	rel=None
+	key=None
+	primaryjoin=None
+	def __init__(self,rel):
+		self.model=rel.mapper.class_
+		self.rel=rel
+		self.key=rel.key
+		self.primaryjoin=rel.primaryjoin
+	
+	def print_info(self):
+		UtilLog.warning(self.rel.primaryjoin)
+		UtilLog.warning(self.rel.direction.name)
+		UtilLog.warning(self.rel.remote_side)
+		UtilLog.warning(self.rel._reverse_property)
+		#dir(relation)
+class ModelInspector:
 	logic_operators = {"and":and_,"or":or_}
 	comparison_operators = {"eq":"__eq__", "lte":"__le__", "lt":"__lt__", "gte":"__ge__", "gt":"__gt__", "ne":"__ne__", "in":"in_", "nin":"not_in", "like":"like", "nlike":"not_like"}
+	model=None
+	columns={}
+	relationships={}
+
+	def __init__(self,model):
+		self.model=model
+		imodel = inspect(model)
+		for c in imodel.columns:
+			self.columns[c.key] = c
+
+		for r in imodel.relationships:
+			self.relationships[r.key] = RelationshipInspector(r)
+
+	def is_column(self,name:str) -> bool:
+		res = name in self.columns
+		UtilLog.debug("%s is column: %s"%(name,res))
+		return res
+
+	def is_rel(self,name:str) -> bool:
+		res = name in self.relationships
+		UtilLog.debug("%s is rel: %s"%(name,res))
+		return res
+
+	def get_column(self,col_name:str):
+		if col_name in self.columns:
+			return	self.columns[col_name]
+		raise Exception("Column %s not found"%col_name)
+
+	def get_rel(self,name:str):
+		if name in self.relationships:
+			return	self.relationships[name]
+		raise Exception("Relation %s not found"%name)
+
+	def get_column_comparator(self,col_name:str,comp_name:str):
+		if comp_name in self.comparison_operators:
+			col		= self.get_column(col_name)
+			comp	= self.comparison_operators[comp_name]
+			return getattr(col,comp)
+		raise Exception("Comparator %s not found"%comp_name)
+
+	def get_expression(self,col_name:str,comp_name:str,value:any):
+		"""
+		usage: ispector.get_expression("name","eq","John")
+
+		returns representation: Model.name == "John"
+
+		"""
+		comp	= self.get_column_comparator(col_name,comp_name)
+		exp		= comp(value)
+
+		return exp
+
+	def __str__(self) -> str:
+		return "ModelInspector(%s)"%self.model.__name__
+
+
+class UtilSQLAlchemy:
 
 	
 
@@ -21,91 +101,75 @@ class UtilSQLAlchemy:
 	def is_native(value):
 		return isinstance(value, (int, float, str, bool))
 	@staticmethod
-	def resolve_logic_oper(logic_func,model,value):
+	def resolve_logic_oper(logic_func,model_insp,value):
 		print("---------------------------------------------------")
-		print("resolve_logic_oper:",logic_func,model,value)
+		print("resolve_logic_oper:",logic_func,model_insp,value)
 		_clauses = []
-		_cols = []
-		if isinstance(value,list):
+		#_cols = []
+		if isinstance(value,list): #for example: logic_func=and   value=[ {"name":"test"}, {"or":[ {"status":"1"}, {"name":"2"} ]} ]}
 			for elem in value:
 				if not isinstance(elem,dict):
-					raise Exception("Invalid %s:[..] operando:%s, must be dict"%(logic_func,elem))
-				UtilSQLAlchemy.resolve_dict(str(logic_func),model,elem,_clauses,_cols)
+					raise Exception("[resolve_logic_oper] Invalid %s:[..] operando:%s, must be dict"%(logic_func,elem))
+				#UtilSQLAlchemy.resolve_dict(str(logic_func),model_insp,elem,_clauses,_cols)
+				UtilSQLAlchemy.resolve_dict(str(logic_func),model_insp,elem,_clauses)
 				
-		elif isinstance(value,dict):
-			UtilSQLAlchemy.resolve_dict(str(logic_func),model,value,_clauses,_cols)
+		elif isinstance(value,dict): # for example: logic_func=and   value={"name":"test","or":[{"status":"1"},{"name":"2"}]}
+			#UtilSQLAlchemy.resolve_dict(str(logic_func),model_insp,value,_clauses,_cols)
+			UtilSQLAlchemy.resolve_dict(str(logic_func),model_insp,value,_clauses)
 		else:
-			raise Exception("Invalid %s:... operando:%s, must be list or dict"%(logic_func,value))
-		print("_cols:",_cols)
-		lc = len(_cols)
-		if lc == 1:
-			_clauses.append(_cols[0])
-		elif lc > 1:
-			_clauses.append(logic_func(*_cols))
-
+			raise Exception("[resolve_logic_oper] Invalid %s:... operando:%s, must be list or dict"%(logic_func,value))
+		# print("_cols:",_cols)
+		# lc = len(_cols)
+		# if lc == 1:
+		# 	_clauses.append(_cols[0])
+		# elif lc > 1:
+		# 	_clauses.append(logic_func(*_cols))
+		UtilLog.warning("_clauses:%s"%_clauses)
 		if len(_clauses) == 1:
 			return _clauses[0]
 		return logic_func(*_clauses)
 
 	@staticmethod
-	def resolve_operator(model,key,val):
+	def resolve_operator(model_insp,col_name,val):
 		print("---------------------------------------------------")
-		print("resolve_operator:",model,key,val)
-		for op,comparator in UtilSQLAlchemy.comparison_operators.items():
-			if op in val:
-				col = getattr(model["cls"],key)
-				exp = getattr(col,comparator)(val[op])
-				return exp
-		raise Exception("Invalid comparacion operator:%s"%val)
+		print("resolve_operator:",model_insp,col_name,val)
+		for comp_name,comparator in ModelInspector.comparison_operators.items():
+			if comp_name in val:
+				# col = getattr(model["cls"],col_name)
+				# exp = getattr(col,comparator)(val[comp_name])
+				# return exp
+				return model_insp.get_expression(col_name,comp_name,val[comp_name])
+		raise Exception("[resolve_operator] Invalid comparacion operator:%s"%val)
 	@staticmethod
-	def resolve_dict(oper_name,model,elem,_clauses,_cols):
+	def resolve_dict(oper_name,model_insp,elem,_clauses):
+	#def resolve_dict(oper_name,model_insp,elem,_clauses,_cols):
 		print("---------------------------------------------------")
-		print("resolve_dict:",oper_name,model,elem,_clauses,_cols)
+		print("resolve_dict:",oper_name,model_insp,elem,_clauses)
 		for key,val in elem.items():
-			if key in model["columns"]:
+			if model_insp.is_column(key):
 				if isinstance(val,list):
-					col = getattr(model["cls"],key).in_(val)
+					#col = getattr(model_insp["cls"],key).in_(val)
+					col = model_insp.get_expression(key,"in",val)
 				elif isinstance(val,dict):
-					col = UtilSQLAlchemy.resolve_operator(model,key,val)
+					col = UtilSQLAlchemy.resolve_operator(model_insp,key,val)
 				else: #elif val is None or UtilSQLAlchemy.is_native(val):
-					col = getattr(model["cls"],key).__eq__(val)
-				_cols.append(col)
-			elif key in UtilSQLAlchemy.logic_operators:
-				logic_func = UtilSQLAlchemy.logic_operators[key]
-				operation_group = UtilSQLAlchemy.resolve_logic_oper(logic_func,model,val).self_group()
+					#col = getattr(model_insp["cls"],key).__eq__(val)
+					col = model_insp.get_expression(key,"eq",val)
+				#_cols.append(col)
+				_clauses.append(col)
+			elif model_insp.is_rel(key):
+				rel = model_insp.get_rel(key)
+				rel_insp = ModelInspector(rel.model)
+				_clauses.append(rel.primaryjoin)
+				UtilSQLAlchemy.resolve_dict("rel",rel_insp,val,_clauses)
+			elif key in ModelInspector.logic_operators:
+				logic_func = ModelInspector.logic_operators[key]
+				operation_group = UtilSQLAlchemy.resolve_logic_oper(logic_func,model_insp,val).self_group()
 				_clauses.append(operation_group)
 			else:
-				raise Exception("Invalid %s operando:%s, must be column name or logic operator"%(oper_name,key))
-
-	# @staticmethod
-	# def resolve_logic_old(query,model):
-	# 	clauses = []
-	# 	for key,value in query.items():
-	# 		if key in model["columns"]:
-
-	# 		elif key in UtilSQLAlchemy.logic_operators:
-	# 			logic_func = UtilSQLAlchemy.logic_operators[key]
-	# 			operation_group = UtilSQLAlchemy.resolve_logic_oper(logic_func,model,value)
-	# 			clauses.append(operation_group)
-	# 		else:
-	# 			raise Exception("Unknown operator: %s"%key)
-	# 	return clauses
-
-
-	# @staticmethod
-	# def resolve_logic_old(op):
-	# 	clauses = []
-	# 	model = {"cls":op["model"],"columns":op["columns"]}
-	# 	for operation in op["operations"]:
-	# 		oper = operation["operator"]
-	# 		value = operation["value"]
-	# 		if oper in UtilSQLAlchemy.logic_operators:
-	# 			logic_func = UtilSQLAlchemy.logic_operators[oper]
-	# 			operation_group = UtilSQLAlchemy.resolve_logic_oper(logic_func,model,value)
-	# 			clauses.append(operation_group)
-	# 		else:
-	# 			raise Exception("Unknown operator: %s"%oper)
-	# 	return clauses
+				msg ="[resolve_dict] Invalid key:%s, must be column, relationships or logic operator"%key
+				UtilLog.error(msg)
+				raise Exception(msg)
 
 	@staticmethod
 	def validate_params(model,params):
@@ -135,46 +199,15 @@ class UtilSQLAlchemy:
 		if p['page'] > 0:
 			p['offset'] = (p['page'] -1) * p['limit']
 		#get valid columns
-		model_col_names = {}
-		columns = model.__dict__['__table__'].columns
-		#print("table		",model.__dict__)
-		import inspect
-		memb = inspect.getmembers(model, lambda a:not(inspect.isroutine(a)))
-		for name,obj in memb:
-			if name is "customer":
-				print("name:%s obj:"%(name),obj.__dict__)
-
-		for col in columns:
-			# print("----------------------------------------------------")
-			# print("\ncol:%s"%col.__dict__)
-			model_col_names[col.name] = 1
-		#separate key/value filters and key in value filters
-		# filter_by_query = {}
-		# filter_in_query = []
-		# filter_logic_query = []
-		# for k,v in p['query'].items():
-		# 	if k in model_col_names:
-		# 		if isinstance(v,list):
-		# 			f = getattr(model,k).in_(v)
-		# 			filter_in_query.append(f)
-		# 		else:
-		# 			filter_by_query[k] = v
-		# 	elif k in UtilSQLAlchemy.logic_operators:
-		# 		filter_logic_query.append({"operator":k,"value":v})
-
-		# p['query'] = filter_by_query
-		# p['query_in'] = filter_in_query
-		# p['query_logic'] = {
-		# 	"operations": filter_logic_query,
-		# 	"columns":model_col_names,
-		# 	"model":model
-		# }
-		p["model"]= {"cls":model,"columns":model_col_names}
+		
+			
+		model_insp = ModelInspector(model)
+		#p["model_insp"]= model_insp
 
 		#create sort by valid columns
 		sort_by = []
 		for k,v in p['sort'].items():
-			if k in model_col_names:
+			if model_insp.is_column(k):
 				if v == "desc":
 					s = getattr(model,k).desc()
 					sort_by.append(s)
@@ -190,17 +223,9 @@ class UtilSQLAlchemy:
 		if validate:
 			p = UtilSQLAlchemy.validate_params(model,p)
 		queryset = model.query
-		# if 'query' in p :
-		# 	queryset = queryset.filter_by(**p['query'])
-		# if 'query_in' in p :
-		# 	queryset = queryset.filter(*p['query_in'])
-		# if 'query_logic' in p :
-		# 	clauses = UtilSQLAlchemy.resolve_logic(p['query_logic'])
-		# 	queryset = queryset.filter(*clauses)
 		if 'query' in p :
-			# clauses = UtilSQLAlchemy.resolve_logic(p['query'],p["model"])
-			# queryset = queryset.filter(*clauses)
-			queryset = queryset.filter( UtilSQLAlchemy.resolve_logic_oper(and_,p['model'],p['query']) )
+			#queryset = queryset.filter( UtilSQLAlchemy.resolve_logic_oper(and_,p['model_insp'],p['query']) )
+			queryset = queryset.filter( UtilSQLAlchemy.resolve_logic_oper(and_,ModelInspector(model),p['query']) )
 		if 'sort' in p :
 			queryset = queryset.order_by(*p['sort'])
 		if limits:
